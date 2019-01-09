203c3bc8b5706771f3829883b308e0fb009357f4
Enda Phelan <ephelan@redhat.com>
2019-01-09 11:50:15 +0000

    Initialised Go code and created folder structure

---
+++ cmd/mobile-security-service-server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/aerogear/mobile-security-service-server/pkg/config"
+	"github.com/labstack/echo"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	config := config.Get()
+	e := echo.New()
+
+	initLogger(config.LogLevel, config.LogFormat)
+
+	log.WithFields(log.Fields{"listenAddress": config.ListenAddress}).Info("Starting application")
+
+	// start webserver
+	if err := e.Start(config.ListenAddress); err != nil {
+		panic("failed to start" + err.Error())
+	}
+}
+
+func initLogger(level, format string) {
+	logLevel, err := log.ParseLevel(level)
+
+	if err != nil {
+		log.Fatalf("log level %v is not allowed. Must be one of [debug, info, warning, error, fatal, panic]", level)
+		logLevel = log.InfoLevel
+	}
+
+	log.SetLevel(logLevel)
+
+	switch format {
+	case "json":
+		log.SetFormatter(&log.JSONFormatter{})
+	case "text":
+		log.SetFormatter(&log.TextFormatter{DisableColors: true})
+	default:
+		log.Fatalf("log format %v is not allowed. Must be one of [text, json]", format)
+	}
+}
