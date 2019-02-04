f6531b1fe4b2dadc516c035d38d8ab543ec511d3
Austin Cunningham <aucunnin@redhat.com>
2019-02-04 14:05:42 +0000

    feat(commit-message-check): added some logic to point out where the issue lies in the commit message (AEROGEAR-8465)

--- scripts/commit-filter-check.sh
+++ scripts/commit-filter-check.sh
@@ -13,14 +13,37 @@ do
   gitmessage=`git log --format=%B -n 1 "$i"`
 
   # fix(commit-filter-check): add commit messages (AEROGEAR-038928990423)
-   messagecheck=`echo $gitmessage | grep -w "feat\|fix\|docs" | grep "(AEROGEAR-" | grep "): "`
+  messagecheck=`echo $gitmessage | grep -w "feat\|fix\|docs"`
+  if [ -z "$messagecheck" ]
+  then 
+      echo "Your commit message must beging with one of the following"
+      echo "  feat(feature-name)"
+      echo "  fix(fix-name)"
+      echo "  docs(docs-change)"
+  fi
+  messagecheck=`echo $gitmessage | grep "(AEROGEAR-"`
+  if  [ -z "$messagecheck" ]
+  then 
+      echo "Your commit message must end with the following"
+      echo "  (AEROGEAR-****)"
+      echo "Where **** is the Jira number"
+  fi
+  messagecheck=`echo $gitmessage | grep "): "`
+  if  [ -z "$messagecheck" ]
+  then 
+      echo "Your commit message has a formatting error please take note of special characters '():' position and use in the example below"
+      echo "   type(some txt): some txt (AEROGEAR-****)"
+      echo "Where 'type' is fix, feat or docs and **** is the Jira number"
+  fi
+
+  messagecheck=`echo $gitmessage | grep -w "feat\|fix\|docs" | grep "(AEROGEAR-" | grep "): "`
+
+ 
 
   # check to see if the messagecheck var is empty
   if [ -z "$messagecheck" ]
-  then 
-        echo "$messagecheck"
-        echo "$gitmessage"
-        echo "failed commit message check"
+  then  
+        echo "'$i' commit message failed"
         rm shafile.txt >/dev/null 2>&1
         set -o errexit
         #break
