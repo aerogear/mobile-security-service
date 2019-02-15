661c0cf2ee2cb1cebe3829a16cadc3cbf0de808c
Camila Macedo <camilamacedo86@gmail.com>
2019-02-15 11:44:25 +0000

    fix(CI): Fix CI to check if the swagger api documentation changes was updated

--- scripts/check-if-swagger-api-file-is-update.sh
+++ scripts/check-if-swagger-api-file-is-update.sh
@@ -8,11 +8,11 @@ check-if-swagger-api-file-is-update (){
     # run the make file task to update the swagger file definition
     make build_swagger_api
     # Check if it is updated
-    if ! cmp api/swagger.yaml api/original.yaml~ >/dev/null 2>&1
+    if ! cmp api/swagger.yaml api/original.yaml >/dev/null 2>&1
     then
-        echo "The REST API Swagger doc was updated and it was not commit."
+        echo "The REST API Swagger doc was updated and it was not committed."
         echo "Please, run 'make setup' or 'build_swagger_api' and commit the changes in the file 'api/swagger.yaml'."
-        set -o errexit
+        exit 1
     fi
 
 }
