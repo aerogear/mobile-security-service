3e2aac997e219e2557d4e0d7d6aa348e2a0109ce
Camila Macedo <camilamacedo86@gmail.com>
2019-02-15 10:53:51 +0000

    task(swagger) - Check in the CI if the swagger doc was updated

---
+++ scripts/check-if-swagger-api-file-is-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# return error if the swagger file was not updated
+check-if-swagger-api-file-is-update (){
+    echo "Checking swagger api documentation ..."
+    # create a copy of the original file
+    cp api/swagger.yaml api/original.yaml
+    # run the make file task to update the swagger file definition
+    make build_swagger_api
+    # Check if it is updated
+    if ! cmp api/swagger.yaml api/original.yaml~ >/dev/null 2>&1
+    then
+        echo "The REST API Swagger doc was updated and it was not commit."
+        echo "Please, run 'make setup' or 'build_swagger_api' and commit the changes in the file 'api/swagger.yaml'."
+        set -o errexit
+    fi
+
+}
+
+# Calling the function
+check-if-swagger-api-file-is-update
