c3dc3d1e228f3029805d0c46cc24d411fbb17032
Enda Phelan <ephelan@redhat.com>
2019-03-21 13:55:21 +0000

    feat(ui): Added JSDoc comments (AEROGEAR-8874)

--- ui/src/components/common/Content.js
+++ ui/src/components/common/Content.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Grid, GridItem } from '@patternfly/react-core';
 
+/**
+ * Stateless component that is a container for the main body of the application
+ *
+ * @param {*} children - Inner HTML and React elements
+ * @param {String} className - Optionally provide a custom class for the component
+ */
 const Content = ({ children, className }) => (
   <Grid className={className}>
     <GridItem span={1} />
