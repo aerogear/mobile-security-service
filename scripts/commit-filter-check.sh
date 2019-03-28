d141b99af5be0da70e2332f049b6e8975216630f
Enda Phelan <ephelan@redhat.com>
2019-03-28 16:41:02 +0000

    ci: separated CI script and Git hook (AEROGEAR-8888)

--- scripts/commit-filter-check.sh
+++ scripts/commit-filter-check.sh
@@ -1,46 +1,72 @@
 #!/bin/bash
 
 commit_message_check (){
-      # gets the content of the current commit message
-      gitmessage="$(cat $1)"
-      
-      matchtypes="feat\|fix\|docs\|breaking\|build\|ci\|perf\|refactor\|style\|test"
-      types="feat, fix, docs, breaking, build, ci, perf, refactor, style, test"
-      
-      messagecheck=`echo $gitmessage | grep -w "$matchtypes"`
+      # Get the current branch and apply it to a variable
+      currentbranch=`git branch | grep \* | cut -d ' ' -f2`
 
+      # Gets the commits for the current branch and outputs to file
+      git log $currentbranch --pretty=format:"%H" --not master > shafile.txt
+
+      # loops through the file an gets the message
+      for i in `cat ./shafile.txt`;
+      do 
+      # gets the git commit message based on the sha
+      gitmessage=`git log --format=%B -n 1 "$i"`
+      
+      ####################### TEST STRINGS comment out line 13 to use #########################################
+      #gitmessage="feat sdasdsadsaas (AEROGEAR-asdsada)"
+      #gitmessage="feat(some txt): some txt (AEROGEAR-****)"
+      #gitmessage="docs(some txt): some txt (AEROGEAR-1234)"
+      #gitmessage="fix(some txt): some txt (AEROGEAR-5678)"
+      #########################################################################################################
+      
+      messagecheck=`echo $gitmessage | grep -w "feat\|fix\|docs\|breaking"`
       if [ -z "$messagecheck" ]
       then 
-            echo -e "Your commit message must begin with one of the following: [\e[33m$types\033[0m]"
+            echo "Your commit message must begin with one of the following"
+            echo "  feat(feature-name)"
+            echo "  fix(fix-name)"
+            echo "  docs(docs-change)"
+            echo " "
       fi
       messagecheck=`echo $gitmessage | grep "(AEROGEAR-"`
       if  [ -z "$messagecheck" ]
       then 
-            echo -e "Your commit message must end with the following: \e[33m(AEROGEAR-****)\033[0m where **** is the JIRA number"
+            echo "Your commit message must end with the following"
+            echo "  (AEROGEAR-****)"
+            echo "Where **** is the Jira number"
             echo " " 
       fi
-
       messagecheck=`echo $gitmessage | grep ": "`
       if  [ -z "$messagecheck" ]
       then 
-            echo "Your commit message has a formatting error please take note of special characters '():' position and use in the example below:"
-            echo -e "\n\t\e[34mtype(optional-scope): some txt (AEROGEAR-****)\033[0m"
+            echo "Your commit message has a formatting error please take note of special characters '():' position and use in the example below"
+            echo "   type(some txt): some txt (AEROGEAR-****)"
+            echo "Where 'type' is fix, feat, docs or breaking and **** is the Jira number"
             echo " "
       fi
 
-      messagecheck=`echo $gitmessage | grep -w "$matchtypes" | grep "(AEROGEAR-" | grep ": "`
+      messagecheck=`echo $gitmessage | grep -w "feat\|fix\|docs\|breaking" | grep "(AEROGEAR-" | grep ": "`
+
+      
 
       # check to see if the messagecheck var is empty
       if [ -z "$messagecheck" ]
       then  
-            echo -e "\e[31m[COMMIT MESSAGE FAILED]\033[0m Please review the following:\n"
+            echo "The commit message with sha: '$i' failed "
+            echo "Please review the following :"
+            echo " "
             echo $gitmessage
             echo " "
+            rm shafile.txt >/dev/null 2>&1
             exit 1
       else
-            echo -e "\e[92m[COMMIT MESSAGE PASSED]\033[0m\n"
+            echo "$messagecheck"
+            echo "'$i' commit message passed"
       fi  
+      done
+      rm shafile.txt  >/dev/null 2>&1
 }
 
 # Calling the function
-commit_message_check $1
+commit_message_check
